91915f141101ff0e1aafcbeffb6bca5c04b76a1e
benmertmiyim <benmertmiyim35@gmail.com>
2022-11-07 20:28:48 +0300

    bugfix

--- lib/ui/auth/login_screen/login_screen.dart
+++ lib/ui/auth/login_screen/login_screen.dart
@@ -12,156 +12,148 @@ class LoginScreen extends StatelessWidget {
     String password = "";
 
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            padding: const EdgeInsets.only(left: 35, top: 130),
-            child: const Text(
-              'Welcome\nBack',
-              style: TextStyle(color: Colors.white, fontSize: 33),
-            ),
-          ),
-          SingleChildScrollView(
-            child: Container(
-              padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.5),
-              child: Form(
-                key: formKey,
+      backgroundColor: const Color(0xffF7EBE1),
+      body:           Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(left: 35, top: 130),
+                child: const Text(
+                  'Welcome\nBack',
+                  style: TextStyle( fontSize: 33),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 35, right: 35),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      margin: const EdgeInsets.only(left: 35, right: 35),
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter email';
-                              } else {
-                                email = value;
-                              }
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter email';
+                        } else {
+                          email = value;
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: "Email",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          )),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter password';
+                        } else {
+                          password = value;
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          )),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Sign in',
+                          style: TextStyle(
+                              fontSize: 27, fontWeight: FontWeight.w700),
+                        ),
+                        Consumer<AuthView>(
+                          builder: (BuildContext context, value,
+                              Widget? child) {
+                            if (value.authProcess == AuthProcess.idle) {
+                              return CircleAvatar(
+                                radius: 30,
+                                backgroundColor: const Color(0xff4c505b),
+                                child: IconButton(
+                                    color: Colors.white,
+                                    onPressed: () async {
+                                      if (formKey.currentState!
+                                          .validate()) {
+                                        await value.signInWithEmailAndPassword(
+                                            email, password).then((customer){
+                                          if(value.customer == null){
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(
+                                              const SnackBar(
+                                                content: Text(
+                                                    'Email or passwrod is wrong!'),),
+                                            );
+                                          }
+                                        });
 
-                              return null;
-                            },
-                            decoration: InputDecoration(
-                                fillColor: Colors.grey.shade100,
-                                filled: true,
-                                hintText: "Email",
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                )),
+                                      }
+                                    },
+                                    icon: const Icon(
+                                      Icons.arrow_forward,
+                                    )),
+                              );
+                            } else {
+                              return const CircularProgressIndicator();
+                            }
+                          },
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, 'register');
+                          },
+                          style: const ButtonStyle(),
+                          child: const Text(
+                            'Sign Up',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Color(0xff4c505b),
+                                fontSize: 18),
                           ),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          TextFormField(
-                            obscureText: true,
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter password';
-                              } else {
-                                password = value;
-                              }
-                              return null;
-                            },
-                            decoration: InputDecoration(
-                                fillColor: Colors.grey.shade100,
-                                filled: true,
-                                hintText: "Password",
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                )),
-                          ),
-                          const SizedBox(
-                            height: 40,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Text(
-                                'Sign in',
-                                style: TextStyle(
-                                    fontSize: 27, fontWeight: FontWeight.w700),
+                        ),
+                        TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Forgot Password',
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Color(0xff4c505b),
+                                fontSize: 18,
                               ),
-                              Consumer<AuthView>(
-                                builder: (BuildContext context, value,
-                                    Widget? child) {
-                                  if (value.authProcess == AuthProcess.idle) {
-                                    return CircleAvatar(
-                                      radius: 30,
-                                      backgroundColor: const Color(0xff4c505b),
-                                      child: IconButton(
-                                          color: Colors.white,
-                                          onPressed: () async {
-                                            if (formKey.currentState!
-                                                .validate()) {
-                                              await value.signInWithEmailAndPassword(
-                                                  email, password).then((customer){
-                                                if(value.customer == null){
-                                                  ScaffoldMessenger.of(context)
-                                                      .showSnackBar(
-                                                    const SnackBar(
-                                                      content: Text(
-                                                          'Email or passwrod is wrong!'),),
-                                                  );
-                                                }
-                                              });
-
-                                            }
-                                          },
-                                          icon: const Icon(
-                                            Icons.arrow_forward,
-                                          )),
-                                    );
-                                  } else {
-                                    return const CircularProgressIndicator();
-                                  }
-                                },
-                              )
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 40,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pushNamed(context, 'register');
-                                },
-                                style: const ButtonStyle(),
-                                child: const Text(
-                                  'Sign Up',
-                                  textAlign: TextAlign.left,
-                                  style: TextStyle(
-                                      decoration: TextDecoration.underline,
-                                      color: Color(0xff4c505b),
-                                      fontSize: 18),
-                                ),
-                              ),
-                              TextButton(
-                                  onPressed: () {},
-                                  child: const Text(
-                                    'Forgot Password',
-                                    style: TextStyle(
-                                      decoration: TextDecoration.underline,
-                                      color: Color(0xff4c505b),
-                                      fontSize: 18,
-                                    ),
-                                  )),
-                            ],
-                          )
-                        ],
-                      ),
+                            )),
+                      ],
                     )
                   ],
                 ),
-              ),
-            ),
+              )
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
